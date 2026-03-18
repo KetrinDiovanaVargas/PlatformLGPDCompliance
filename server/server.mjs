b4c6193e11b27031aa1b4a63c0b09e029ab80a50
Ketrin Vargas <ketrin.diovana.vargas@gmail.com>
2026-03-18 08:53:09 -0300

    correcao

--- server/server.mjs
+++ server/server.mjs
@@ -42,7 +42,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+// app.options("/*", cors(corsOptions)); // só adicione se precisar mesmo
 
 app.use(express.json());
 
